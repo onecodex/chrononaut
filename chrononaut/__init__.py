cbdbf5e4efacb04c1c10e25d7128a9f12a186790
Paweł Konieczny <pawel@onecodex.com>
2021-01-20 16:12:42 +0100

    Add migrations from history table model

--- chrononaut/__init__.py
+++ chrononaut/__init__.py
@@ -25,6 +25,7 @@ from chrononaut.exceptions import ChrononautException
 from chrononaut.flask_versioning import create_version
 from chrononaut.versioned import Versioned
 from chrononaut.models import activity_factory
+from chrononaut.migrations import MigrateFromHistoryTableOp, MigrateToHistoryTableOp
 
 
 def versioned_objects(items):
@@ -110,4 +111,6 @@ __all__ = [
     "extra_change_info",
     "rationale",
     "ChrononautException",
+    "MigrateFromHistoryTableOp",
+    "MigrateToHistoryTableOp",
 ]
---
+++ chrononaut/migrations.py
@@ -0,0 +1,68 @@
+from alembic.operations import Operations, MigrateOperation
+from chrononaut import ChrononautException
+
+@Operations.register_operation("migrate_from_history_table")
+class MigrateFromHistoryTableOp(MigrateOperation):
+    """
+    Migrate a single history table into the single activity model. Pass in base table
+    name to migrate from (without the ``_history`` suffix).
+    """
+
+    def __init__(self, table_name, schema=None):
+        self.schema = schema
+        self.table_name = table_name
+    
+    @classmethod
+    def migrate_from_history_table(cls, operations, table_name, **kwargs):
+        op = MigrateFromHistoryTableOp(table_name, **kwargs)
+        return operations.invoke(op)
+
+    def reverse(self):
+        return MigrateToHistoryTableOp(self.table_name, schema=self.schema)
+
+
+@Operations.register_operation("migrate_to_history_table")
+class MigrateToHistoryTableOp(MigrateOperation):
+    """
+    Migrate activity entries into a history table. Pass in base table name
+    to migrate to (without the ``_history`` suffix).
+    """
+
+    def __init__(self, table_name, schema=None):
+        self.schema = schema
+        self.table_name = table_name
+    
+    @classmethod
+    def migrate_to_history_table(cls, operations, table_name, **kwargs):
+        op = MigrateToHistoryTableOp(table_name, **kwargs)
+        return operations.invoke(op)
+
+    def reverse(self):
+        return MigrateFromHistoryTableOp(self.table_name, schema=self.schema)
+
+
+@Operations.implementation_for(MigrateFromHistoryTableOp)
+def migrate_from_history_table(operations, operation):
+    activity_table = "activity" if not operation.schema else f"{operation.schema}.activity"
+    table_name = (
+        operation.table_name.replace("_history", "") 
+        if operation.table_name.endswith("_history")
+        else operation.table_name
+    )
+    history_table = table_name + "_history"
+
+    sql = (
+        "INSERT INTO {}(table_name, changed, version, data, user_info, extra_info) "
+        "SELECT '{}', changed, version, "
+        "row_to_json({}.*)::jsonb #- '{{change_info}}' #- '{{changed}}', "
+        "change_info #- '{{extra}}', coalesce(change_info->'extra', '{{}}')::jsonb "
+        "FROM {} ORDER BY changed ASC"
+    ).format(activity_table, table_name, history_table, history_table)
+
+    conn = operations
+    conn.execute(sql)
+
+
+@Operations.implementation_for(MigrateToHistoryTableOp)
+def migrate_to_history_table(operations, operation):
+    raise ChrononautException("Migrating back to history tables is not supported")
