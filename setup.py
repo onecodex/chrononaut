147a5721dfa7dde9da55b13803ded6ad7ab3ac70
Paweł Konieczny <pawel@onecodex.com>
2023-05-05 11:30:00 +0200

    Compare based on `changed` instead of `version` (#32)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ adding support for audit logging, record locking, and time travel!
 from setuptools import setup, find_packages
 import sys
 
-__version__ = "0.4.0"
+__version__ = "0.4.2"
 
 
 if sys.version_info[0] < 3:
